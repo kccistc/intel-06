78f747a69725edf324735e64e6de7ac0a12dc1cd
JongBeom Lee <dlwhdqja1128@gmail.com>
2025-04-14 16:28:21 +0900

    class01-hw4-whdqja1128

--- class01/homework/LJB/hw4_pythonHW_and_math/hw4_GD.py
+++ class01/homework/LJB/hw4_pythonHW_and_math/hw4_GD.py
@@ -183,48 +183,48 @@ def contour_with_path(f, x, y, paths, norm=LogNorm(), level=np.logspace(0, 5, 35
 
 	plt.show()
 	
-# xmin, xmax, xstep = -4.0, 4.0, .25
-# ymin, ymax, ystep = -4.0, 4.0, .25
+xmin, xmax, xstep = -4.0, 4.0, .25
+ymin, ymax, ystep = -4.0, 4.0, .25
 
-# x, y = np.meshgrid(np.arange(xmin, xmax + xstep, xstep),
-#                    np.arange(ymin, ymax + ystep, ystep))
+x, y = np.meshgrid(np.arange(xmin, xmax + xstep, xstep),
+                   np.arange(ymin, ymax + ystep, ystep))
 
-# f = lambda x, y: (x-2)**2 + (y-2)**2
-# z = f(x, y)
-# minima = np.array([2, 2])
+f = lambda x, y: (x-2)**2 + (y-2)**2
+z = f(x, y)
+minima = np.array([2, 2])
 
-# f(*minima)
+f(*minima)
 
-# minima_ = minima.reshape(-1, 1)
-# print(minima, minima_)
-# surf(f, x, y, minima=minima_)
+minima_ = minima.reshape(-1, 1)
+print(minima, minima_)
+surf(f, x, y, minima=minima_)
 
-# grad_f_x = lambda x, y: 2*(x-2)
-# grad_f_y = lambda x, y: 2*(y-2)
+grad_f_x = lambda x, y: 2*(x-2)
+grad_f_y = lambda x, y: 2*(y-2)
 
-# contour_with_quiver(f, x, y, grad_f_x, grad_f_y, minima=minima_)
+contour_with_quiver(f, x, y, grad_f_x, grad_f_y, minima=minima_)
 
-# def steepest_descent_twod(func, gradx,  grady, x0, Maxlter = 10, learning_rate = 0.25, verbose = True):
-# 	paths = [x0]
-# 	fval_paths = [f(x0[0], x0[1])]
-# 	for i in range(Maxlter):
-# 		grad = np.array([grad_f_x(*x0), grad_f_y(*x0)])
-# 		x1 = x0 - learning_rate * grad
-# 		fval = f(*x1)
-# 		if verbose:
-# 			print(i, x1, fval)
-# 		x0 = x1
-# 		paths.append(x0)
-# 		fval_paths.append(fval)
-# 	paths = np.array(paths)
-# 	paths = np.array(np.matrix(paths).T)
-# 	fval_paths = np.array(fval_paths)
-# 	return (x0, fval, paths, fval_paths)
+def steepest_descent_twod(func, gradx,  grady, x0, Maxlter = 10, learning_rate = 0.25, verbose = True):
+	paths = [x0]
+	fval_paths = [f(x0[0], x0[1])]
+	for i in range(Maxlter):
+		grad = np.array([grad_f_x(*x0), grad_f_y(*x0)])
+		x1 = x0 - learning_rate * grad
+		fval = f(*x1)
+		if verbose:
+			print(i, x1, fval)
+		x0 = x1
+		paths.append(x0)
+		fval_paths.append(fval)
+	paths = np.array(paths)
+	paths = np.array(np.matrix(paths).T)
+	fval_paths = np.array(fval_paths)
+	return (x0, fval, paths, fval_paths)
 
-# x0 = np.array([-2., -2.])
-# xopt, fopt, paths, fval_paths = steepest_descent_twod(f, grad_f_x, grad_f_y, x0)
+x0 = np.array([-2., -2.])
+xopt, fopt, paths, fval_paths = steepest_descent_twod(f, grad_f_x, grad_f_y, x0)
 
-# contour_with_path(f, x, y, paths, minima=np.array([[2], [2]]))
+contour_with_path(f, x, y, paths, minima=np.array([[2], [2]]))
 
 # GD vs Momentum
 
