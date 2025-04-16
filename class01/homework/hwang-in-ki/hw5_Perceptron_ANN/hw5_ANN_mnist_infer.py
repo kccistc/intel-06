f76680e4833ce2f43a2c9dcc39c785dceadb882d
Azruine <tyriba967@gmail.com>
2025-04-16 16:08:32 +0900

    Class01_hw5_HIK

---
+++ class01/homework/hwang-in-ki/hw5_Perceptron_ANN/hw5_ANN_mnist_infer.py
@@ -0,0 +1,27 @@
+import tensorflow as tf
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+matplotlib.use('TkAgg')
+
+model = tf.keras.models.load_model('fashion_mnist_model.h5')
+
+num = 10
+fashion_mnist = tf.keras.datasets.fashion_mnist
+(f_image_train, f_label_train), (f_image_test,
+                                 f_label_test) = fashion_mnist.load_data()
+f_image_train, f_image_test = f_image_train / 255.0, f_image_test / 255.0
+predict = model.predict(f_image_test[:num])
+print(" *    Data:   ", f_label_test[:num])
+print(" * Predicted: ", np.argmax(predict, axis=1))
+plt.figure(figsize=(10, 10))
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+for i in range(num):
+    plt.subplot(3, 4, i + 1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid(False)
+    plt.imshow(f_image_test[i], cmap=plt.cm.binary)
+    plt.xlabel(class_names[f_label_test[i]])
+plt.show()
