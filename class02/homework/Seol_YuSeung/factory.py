2a9a1ab505c9386f84657d6305cbe480d3078c21
Yuseung Seol <sus990415@naver.com>
2025-04-29 16:49:03 +0900

    class 02 hw2

--- class02/homework/Seol_YuSeung/factory.py
+++ class02/homework/Seol_YuSeung/factory.py
@@ -5,10 +5,11 @@ import threading
 from argparse import ArgumentParser
 from queue import Empty, Queue
 from time import sleep
+from pathlib import Path
 
 import cv2
 import numpy as np
-# from openvino.inference_engine import IECore
+import openvino as ov
 
 from iotdemo import FactoryController, MotionDetector, ColorDetector
 
@@ -16,41 +17,62 @@ FORCE_STOP = False
 
 
 def thread_cam1(q):
-    # TODO: MotionDetector
+    # 모션 감지기 초기화
     Motion1 = MotionDetector()
     Motion1.load_preset('resources/motion.cfg')
 
-    # TODO: Load and initialize OpenVINO
+    # OpenVINO 로드 및 초기화
+    device = "CPU"
 
-    # TODO: HW2 Open video clip resources/conveyor.mp4 instead of camera device.
+    base_artifacts_dir = Path("resources").expanduser()
+    model_name = "model"
+    model_xml_name = f"{model_name}.xml"
+    model_xml_path = base_artifacts_dir / model_xml_name
+
+    core = ov.Core()
+    model = core.read_model(model=model_xml_path)
+    compiled_model = core.compile_model(model=model, device_name=device)
+    output_layer = compiled_model.output(0)
+    
+    # 비디오 파일 열기
     cap = cv2.VideoCapture('resources/conveyor.mp4')
+    
     while not FORCE_STOP:
         sleep(0.03)
         _, frame = cap.read()
         if frame is None:
             break
 
-        # TODO: HW2 Enqueue "VIDEO:Cam1 live", frame info
+        
         q.put(('VIDEO:Cam1 live', frame))
-        # TODO: Motion detect
+        
+        
         detected = Motion1.detect(frame)
         if detected is None:
             continue
-        # TODO: Enqueue "VIDEO:Cam1 detected", detected info.
+            
+        
         q.put(('VIDEO:Cam1 detected', detected))
-        # abnormal detect
-        # frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        # reshaped = detected[:, :, [2, 1, 0]]
-        # np_data = np.moveaxis(reshaped, -1, 0)
-        # preprocessed_numpy = [((np_data / 255.0) - 0.5) * 2]
-        # batch_tensor = np.stack(preprocessed_numpy, axis=0)
+        
+        
+        frame = cv2.cvtColor(detected, cv2.COLOR_BGR2RGB)
+        reshaped = frame[:, :, [2, 1, 0]]
+        np_data = np.moveaxis(reshaped, -1, 0)
+        preprocessed_numpy = [((np_data / 255.0) - 0.5) * 2]
+        batch_tensor = np.stack(preprocessed_numpy, axis=0)
 
-        # TODO: Inference OpenVINO
+        
+        results = compiled_model([batch_tensor])[output_layer]
+        
+       
+        x_ratio = results[0][0] * 100
+        circle_ratio = results[0][1] * 100
+        
+        print(f"X = {x_ratio:.2f}%, Circle = {circle_ratio:.2f}%")
 
-        # TODO: Calculate ratios
-        # print(f"X = {x_ratio:.2f}%, Circle = {circle_ratio:.2f}%")
-
-        # TODO: in queue for moving the actuator 1
+        
+        if x_ratio > 60:
+            q.put(("PUSH", 1))
 
     cap.release()
     q.put(('DONE', None))
@@ -58,15 +80,15 @@ def thread_cam1(q):
 
 
 def thread_cam2(q):
-    # TODO: MotionDetector
+    
     Motion2 = MotionDetector()
     Motion2.load_preset('resources/motion.cfg')
 
-    # TODO: ColorDetector
+    
     Color2 = ColorDetector()
     Color2.load_preset('resources/color.cfg')
 
-    # TODO: HW2 Open "resources/conveyor.mp4" video clip
+    
     cap = cv2.VideoCapture('resources/conveyor.mp4')
 
     while not FORCE_STOP:
@@ -75,20 +97,24 @@ def thread_cam2(q):
         if frame is None:
             break
 
-        # TODO: HW2 Enqueue "VIDEO:Cam2 live", frame info
+        
         q.put(('VIDEO:Cam2 live', frame))
-        # TODO: Detect motion
+        
+        
         detected = Motion2.detect(frame)
         if detected is None:
             continue
-        # TODO: Enqueue "VIDEO:Cam2 detected", detected info.
+            
+        
         q.put(('VIDEO:Cam2 detected', detected))
-        # TODO: Detect color
+        
+        
         name, ratio = Color2.detect(detected)[0]
-        # TODO: Compute ratio
-        print(f"{name}: {ratio*100:2f}%")
+        
+        
+        print(f"{name}: {ratio*100:.2f}%")
 
-        # TODO: Enqueue to handle actuator 2
+        
         if name == 'blue':
             q.put(("PUSH", 2))
 
@@ -117,10 +143,10 @@ def main():
                         help="Arduino port")
     args = parser.parse_args()
 
-    # TODO: HW2 Create a Queue
+    
     q = Queue()
 
-    # TODO: HW2 Create thread_cam1 and thread_cam2 threads and start them.
+    
     cam1 = threading.Thread(target=thread_cam1, args=(q,))
     cam2 = threading.Thread(target=thread_cam2, args=(q,))
     cam1.start()
@@ -131,34 +157,31 @@ def main():
             if cv2.waitKey(10) & 0xff == ord('q'):
                 break
 
-            # TODO: HW2 get an item from the queue. You might need to properly handle exceptions.
-            # de-queue name and data
+            
             try:
                 event = q.get_nowait()
             except Empty:
                 continue
+                
             name, data = event
 
-            # TODO: HW2 show videos with titles of 'Cam1 live' and 'Cam2 live' respectively.
+            
             if name[-4:] == 'live':
-                imshow(name[-9:], data, pos=None)
+                imshow(name[6:], data, pos=None)
             elif name[-8:] == 'detected':
-                imshow(name[-13:], data, pos=None)
-
-
-            # TODO: Control actuator, name == 'PUSH'
-
+                imshow(name[6:], data, pos=None)
+            
             elif name == 'PUSH':
                 ctrl.push_actuator(data)
             elif name == 'DONE':
                 FORCE_STOP = True
 
             q.task_done()
-    cam1.join
+            
+    cam1.join()
     cam2.join()
 
     cv2.destroyAllWindows()
-
 
 if __name__ == '__main__':
     try:
